cffb1715c3473cf3396e0b65bebe4c28f928d830
teukumufti <teukumufti9770@gmail.com>
2022-04-08 14:48:40 +0700

    ADD: db

--- conection/db.js
+++ conection/db.js
@@ -11,12 +11,10 @@ if (process.env.DEV) {
   });
 } else {
   client = new Client({
-    host: "ec2-52-54-212-232.compute-1.amazonaws.com",
-    database: "d7pk27unm9sl21",
-    user: "jeirkfsmmwdywg",
-    port: 5432,
-    password:
-      "c30e4ad072c30a727c9e8ecaa4495aa715d8b1374f8de761c1ae2a2a3f209c00",
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+    },
   });
 }
 
